95b5296e8894b301e4a094eed1f483300e28b696
gisberto1712 <106213220+gisberto1712@users.noreply.github.com>
2022-07-12 22:36:03 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+
+const pathPublic = path.resolve(__dirname, './public');
+app.use(express.static(pathPublic));
+
+app.get('/', (req,res)=>{
+    res.sendFile(__dirname + '/views/home.html');
+});
+
+app.post('/', (req, res) => {
+    res.redirect('/');
+});
+
+app.get('/register', (req,res)=>{
+    res.sendFile(__dirname + '/views/register.html');
+});
+
+app.get('/login', (req,res)=>{
+    res.sendFile(__dirname + '/views/login.html');
+});
+
+app.listen(port, ()=>{
+    console.log('Servidor funcionando en: http://localhost:'+ port);
+});
